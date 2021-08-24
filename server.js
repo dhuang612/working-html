8010b58c5e644793fc409e93c2a81a2c3831bae7
dhuang612 <dhuang@madisoncollege.edu>
2021-08-24 13:35:13 -0500

    updating settings for heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var liveServer = require('live-server');
 
 var params = {
-  port: 8181,
+  port: 8181 || process.env.PORT,
+  host: "0.0.0.0" || process.env.IP,
   file:'pass.html'
 }
 liveServer.start(params)
